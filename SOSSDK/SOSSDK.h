cc95471ee7cc85d498e96526cde32c2ff51dc4b6
pqbf4t <Shenkan1Zhao@ShanghaiOnstar.com>
2024-07-18 10:56:34 +0800

    Initial Commit

---
+++ SOSSDK/SOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  SOSSDK.h
+//  SOSSDK
+//
+//  Created by kylin on 2024/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SOSSDK.
+FOUNDATION_EXPORT double SOSSDKVersionNumber;
+
+//! Project version string for SOSSDK.
+FOUNDATION_EXPORT const unsigned char SOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOSSDK/PublicHeader.h>
+
+
